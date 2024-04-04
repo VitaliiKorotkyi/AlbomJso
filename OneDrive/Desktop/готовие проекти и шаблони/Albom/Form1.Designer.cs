4d4d148014428599022dc1c08e0dfd017a45802d
VitaliiKorotkyi <164013441+VitaliiKorotkyi@users.noreply.github.com>
2024-04-04 14:22:33 +0200

    Добавлен проект Albom

---
+++ OneDrive/Desktop/готовие проекти и шаблони/Albom/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Albom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1085, 837);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "=>";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1085, 24);
+            this.panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Location = new System.Drawing.Point(503, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "30 sec ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1085, 837);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ OneDrive/Desktop/готовие проекти и шаблони/Albom/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Drawing;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Albom
+{
+    public partial class Form1 : Form
+    {
+        private const string ImageUrl1 = "https://source.unsplash.com/random";
+        private const string ImageUrl2 = "https://source.unsplash.com/random";
+        private const string DefaultImageUrl = "https://source.unsplash.com/random";
+        private Timer timer;
+        private int secondsLeft = 30;
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            timer1 = new Timer();
+            timer1.Interval = 30000; // Устанавливаем интервал таймера в 30 секунд (30000 миллисекунд)
+            timer1.Tick += async (sender, e) => await UpdateImageAsync();
+        }
+        private async Task UpdateImageAsync()
+        {
+            Image image = await DownloadImageAsync(DefaultImageUrl);
+            if (image != null)
+            {
+                // Удалите предыдущие изображения перед добавлением нового
+                flowLayoutPanel1.Controls.Clear();
+                AddImageToGallery(image);
+                secondsLeft = 30;
+            }
+        }
+        private async Task<Image> DownloadImageAsync(string imageUrl)
+        {
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    var response = await httpClient.GetAsync(imageUrl);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var stream = await response.Content.ReadAsStreamAsync();
+                        return Image.FromStream(stream);
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки изображения: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+
+        private void AddImageToGallery(Image image)
+        {
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Image = image;
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom; // Установите режим масштабирования в Zoom
+            pictureBox.Width = 1500; // Установите желаемую ширину
+            pictureBox.Height = 840; // Установите желаемую высоту
+            flowLayoutPanel1.Controls.Clear(); // Очистите все предыдущие изображения
+            flowLayoutPanel1.Controls.Add(pictureBox); // Добавьте новое изображение
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            Image image = await DownloadImageAsync(ImageUrl1);
+            if (image != null)
+            {
+                AddImageToGallery(image);
+            }
+        }
+
+        private async void label1_Click(object sender, EventArgs e)
+        {
+            Image image = await DownloadImageAsync(ImageUrl2); // Загружаем новое изображение
+            if (image != null)
+            {
+                AddImageToGallery(image); // Заменяем текущее изображение в галерее
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            timer1.Start(); // Запустить таймер при нажатии на лейбл
+        }
+    }
+}
